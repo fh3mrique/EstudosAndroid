2a2df9c89a31980db3b8d7780345ca2f713e784b
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-07-23 00:47:57 -0300

    Componente: Button

--- COMPONENTES/Buttons/app/src/main/java/com/example/buttons/MainActivity.java
+++ COMPONENTES/Buttons/app/src/main/java/com/example/buttons/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.buttons;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean botaoFoiCliclado = false;
+    private String originalButtonText = "Clique aqui";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.tv);
+        Button btnAdd = findViewById(R.id.btnAdd);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (botaoFoiCliclado){
+                    tv.setVisibility(View.GONE);
+                    btnAdd.setText("Clicou");
+                }
+                else {
+                    tv.setVisibility(View.VISIBLE);
+                    btnAdd.setText(originalButtonText);
+                }
+
+                botaoFoiCliclado = !botaoFoiCliclado;
+            }
+        });
     }
 }
