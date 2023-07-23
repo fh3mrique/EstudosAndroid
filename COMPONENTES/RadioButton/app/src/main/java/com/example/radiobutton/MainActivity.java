dc9f9870b36454fa6beae4740b8b3b5627ee7874
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-07-23 19:14:19 -0300

    Componente: RadioButton e RadioGroup

--- COMPONENTES/RadioButton/app/src/main/java/com/example/radiobutton/MainActivity.java
+++ COMPONENTES/RadioButton/app/src/main/java/com/example/radiobutton/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.radiobutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RadioGroup radioGrupo = findViewById(R.id.radioGrupo);
+
+        radioGrupo.setOnCheckedChangeListener((group, checkedId) -> {
+            if (checkedId == R.id.opcao1){
+                Toast.makeText(this, "Opção 1", Toast.LENGTH_SHORT).show();
+            }
+            else if (checkedId == R.id.opcao2){
+                Toast.makeText(this, "Opção 2", Toast.LENGTH_LONG).show();
+            }
+            else if (checkedId == R.id.opcao3){
+                Toast.makeText(this, "Opção 3", Toast.LENGTH_LONG).show();
+            }
+            else{
+                Toast.makeText(this, "Opção 4", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
